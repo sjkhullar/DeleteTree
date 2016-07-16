5d575a9db2cbf7ff3b1b7120052652ef88ec65e1
makeinjava <makeinjava@gmail.com>
2016-07-16 15:08:33 +0530

    Updated code

---
+++ DeleteTree(DFS)/src/main/java/org/learn/Client/App.java
@@ -0,0 +1,47 @@
+package org.learn.Client;
+
+import org.learn.Question.DeleteTree;
+import org.learn.Question.Node;
+
+public class App 
+{
+	public static void main( String[] args )
+    {
+		// root level 0
+		Node A = Node.createNode(60);
+		// Level 1
+		Node B = Node.createNode(20);
+		Node C = Node.createNode(80);
+		// Level 2
+		Node D = Node.createNode(10);
+		Node E = Node.createNode(30);
+		Node F = Node.createNode(70);
+		Node G = Node.createNode(90);
+		// Level 3
+		Node H = Node.createNode(65);
+		Node I = Node.createNode(75);
+		Node J = Node.createNode(85);
+		Node K = Node.createNode(95);
+		
+		// connect Level 0 and 1
+		A.left = B;
+		A.right = C;
+		// connect level 1 and level 2
+		B.left = D;
+		B.right = E;
+		C.left = F;
+		C.right = G;
+		// connect level 2 and level 3
+		F.left  = H;
+		F.right = I;
+		G.left  = J;
+		G.right = K;
+		
+    	Node node = DeleteTree.deleteTree(A);
+    	if(null == node) {
+    		System.out.println("Binary tree deleted successfully");
+    	} else {
+    		System.out.println("Error: Could not delete tree successfully");
+    	}
+    }
+}
